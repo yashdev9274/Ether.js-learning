6690d77663e51d9a530bccac70b872e36142cdf4
Yash Dewasthale <72986960+yashdev9274@users.noreply.github.com>
2023-05-31 22:02:54 +0530

    add a js file

---
+++ 1_accounts.js
@@ -0,0 +1,13 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID = ''
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+
+const address = '0xdafea492d9c6733ae3d56b7ed1adb60692c98bc5'
+
+const main = async () => {
+    const balance = await provider.getBalance(address)
+    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
+}
+
+main()
